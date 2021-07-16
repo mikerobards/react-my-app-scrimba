571a2fa7e4d515220410b891ddcb6751d7d08bc5
Michael Robards <mikerobards@icloud.com>
2021-07-16 16:50:02 -0400

    more fun with inline styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-// import Navbar from '../components/Navbar.jsx'
-// import MainContent from '../components/MainContent.jsx'
-// import Footer from '../components/Footer.jsx'
-import Navbar from './components/Navbar';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+
+// import Navbar from './components/Navbar';
+// import MainContent from './components/MainContent';
+// import Footer from './components/Footer';
 import MyInfo from './components/MyInfo';
 
 function App() {
--- src/components/MyInfo.jsx
+++ src/components/MyInfo.jsx
@@ -4,20 +4,26 @@ function MyInfo() {
   const date = new Date();
   const hours = date.getHours();
   let timeOfDay;
+  const styles = {
+    fontSize: 30,
+  };
 
   if (hours < 12) {
     timeOfDay = "morning";
+    styles.color = "yellow";
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "afternoon";
+    styles.color = "blue";
   } else {
     timeOfDay = "night";
+    styles.color = "red";
   }
 
-  const styles = {
-    color: "#FF8C00",
-    backgroundColor: "#FF2D00",
-    fontSize: "50px",
-  };
+  // const styles = {
+  //   color: "#FF8C00",
+  //   backgroundColor: "#FF2D00",
+  //   fontSize: "50px",
+  // };
 
   return (
     <div>
