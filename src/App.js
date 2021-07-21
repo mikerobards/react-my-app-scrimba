ac071e8e5c5cf52d5d091c4ba0de50c99de8c449
Michael Robards <mikerobards@icloud.com>
2021-07-21 07:14:17 -0400

    add props to todo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
+import React from 'react'
 import './App.css';
+
 
 // import Navbar from './components/Navbar';
 // import MainContent from './components/MainContent';
 // import Footer from './components/Footer';
 // import MyInfo from './components/MyInfo';
-// import TodoItem from './components/TodoItem';
-import ContactCard from './components/ContactCard';
+import TodoItem from './components/TodoItem';
+// import ContactCard from './components/ContactCard';
+import todosData from './data/todosData';
 
 
 function App() {
+  const todoComponents = todosData.map(item =>
+    <TodoItem todoItem={item} key={item.id} />)
   return (
-    <div className="contacts">
-      <ContactCard
-        contact={{ name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow" }}
-      />
-
-      <ContactCard
-        contact={{ name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com" }}
-      />
-
-      <ContactCard
-        contact={{ name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com" }}
-      />
-
-      <ContactCard
-        contact={{ name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com" }}
-      />
+    <div className="todo-list">
+      {todoComponents}
     </div>
   )
 }
