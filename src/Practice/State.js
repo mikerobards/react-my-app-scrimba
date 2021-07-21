66fc2276de82546b20e99d50cfa814905023e8f2
Michael Robards <mikerobards@icloud.com>
2021-07-21 17:42:33 -0400

    setState!

--- src/Practice/State.js
+++ src/Practice/State.js
@@ -4,15 +4,24 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            answer: "Yes!"
+            count: 0
         }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
     }
 
     render() {
         return (
             <div>
-                <h1>Is state important to know? {this.state.answer}</h1>
-                {/* <ChildComponent answer={this.state.answer} /> */}
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
             </div>
         )
     }
