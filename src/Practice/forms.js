77e7017c95da24e14d764cf4ea6119bf2bb0e28e
Michael Robards <mikerobards@icloud.com>
2021-07-23 16:54:57 -0400

    forms lesson

--- src/Practice/forms.js
+++ src/Practice/forms.js
@@ -1,29 +1,96 @@
 import React, { Component } from "react"
 
+// Formik Library!!!
 class App extends Component {
     constructor() {
         super()
         this.state = {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isFriendly: true,
+            gender: "",
+            favColor: "red"
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
     }
 
     handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
+        const { name, value, type, checked } = event.target
+        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+    }
 
-        })
+    handleSubmit() {
+
     }
 
     render() {
         return (
-            <form>
-                <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    value={this.state.firstName}
+                    name="firstName"
+                    placeholder="First Name"
+                    onChange={this.handleChange} />
                 <br />
-                <input type="text" name="lastName" placeholder="First Name" onChange={this.handleChange} />
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
+                <input
+                    type="text"
+                    value={this.state.lastName}
+                    name="lastName"
+                    placeholder="First Name"
+                    onChange={this.handleChange} />
+
+                <textarea
+                    value={"Some default value"}
+                    onChange={this.handleChange} />
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        name="isFriendly"
+                        checked={this.state.isFriendly}
+                        onChange={this.handleChange}
+                    /> Is Friendly?
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={this.state.gender === "male"}
+                        onChange={this.handleChange}
+                    /> Male?
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={this.state.gender === "female"}
+                        onChange={this.handleChange}
+                    /> Female?
+                </label>
+                <h2>{this.state.gender}</h2>
+                <br />
+                <label>Favorite Color: </label>
+                <select
+                    value={this.state.favColor}
+                    onChange={this.handleChange}
+                    name="favColor"
+                >
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                </select>
+
+                <h2>{this.state.favColor</h2>
+                <button>Submit</button>
             </form>
         )
     }
